18cd729d33713055e00d8c2e6e5948fd7f164fad
Cod3Bende4 <mnsikarwar@gmail.com>
2022-02-20 23:02:04 +0530

    Few files added

--- blackmart.py
+++ blackmart.py
@@ -1,15 +1,28 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from typing import Callable
+
+
+class MySQLAlchemy(SQLAlchemy):
+    Column: Callable  # Use the typing to tell the IDE what the type is.
+    String: Callable
+    Integer: Callable
+
 
 app = Flask(__name__)
-
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db1.db'
+db = MySQLAlchemy(app)
 
 
 class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     price = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(length=1024), nullable=False)
+
+    def __repr__(self):
+        return f'Name: {self.name} \n Barcode: {self.barcode}, Price: {self.price} \n'
 
 
 @app.route('/')
@@ -27,15 +40,9 @@ def register():
     return render_template('register.html')
 
 
-items = [
-    {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-    {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-    {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-]
-
-
 @app.route('/market')
 def market():
+    items = Item.query.all()
     return render_template('market.html', items=items)
 
 
@@ -45,7 +52,7 @@ def market():
 def anything(anything_else=None):
     anything_else = ''.join(e for e in anything_else if e.isalnum() or e in "._")
     if len(anything_else) < 10:
-        return render_template('anything_else.html', anything_else=anything_else)
+        return render_template('default.html', anything_else=anything_else)
     else:
-        return render_template('anything_else.html', anything_else=anything_else[:9] + '...')
+        return render_template('default.html', anything_else=anything_else[:9] + '...')
     # return f'<h2>\"{anything_else}\", Seriously !!</h2>'
